1a6c3f0cc7609ffd0043dd5ec75066ff8a75e1a1
jvandijk17 <jvandijkgarcia@gmail.com>
2024-07-31 18:58:38 +0200

    Refactor validators to use go-playground validator v10.

--- domain/book.go
+++ domain/book.go
@@ -4,13 +4,13 @@ import "gorm.io/gorm"
 
 type Book struct {
 	gorm.Model
-	LibraryID       uint       `gorm:"not null"`
-	Title           string     `gorm:"size:255;not null"`
-	Author          string     `gorm:"size:255;not null"`
-	Publisher       string     `gorm:"size:255;not null"`
-	ISBN            string     `gorm:"size:13;not null"`
-	PublicationYear string     `gorm:"type:date;not null"`
-	PageCount       uint       `gorm:"not null"`
+	LibraryID       uint       `gorm:"not null" json:"libraryId" validate:"required"`
+	Title           string     `gorm:"size:255;not null" json:"title" validate:"required"`
+	Author          string     `gorm:"size:255;not null" json:"author" validate:"required"`
+	Publisher       string     `gorm:"size:255;not null" json:"publisher" validate:"required"`
+	ISBN            string     `gorm:"size:13;not null" json:"isbn" validate:"required,isbn"`
+	PublicationYear string     `gorm:"type:date;not null" json:"publication_year" validate:"required,datetime=2006-01-02"`
+	PageCount       uint       `gorm:"not null" json:"pageCount" validate:"required,gt=0"`
 	Categories      []Category `gorm:"many2many:book_categories;"`
 	Loans           []Loan     `gorm:"foreignKey:BookID"`
 }
